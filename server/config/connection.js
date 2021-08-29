d70a5b326495b560568b79d50600786246b7eac5
Beau Barrier <beaubarrier@gmail.com>
2021-08-28 18:09:12 -0700

    completing heroku deploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/graphql', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://beaubarrier:$Cowleg$17@cluster0.hxmal.mongodb.net/book-search-engine?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
