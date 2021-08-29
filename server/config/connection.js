07963aa4b52df17a53590c4da6a0e40ba753b09d
Beau Barrier <beaubarrier@gmail.com>
2021-08-28 18:37:44 -0700

    troubleshoot

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://beaubarrier@gmail.com:$Cowleg$17@cluster0.hxmal.mongodb.net/fierce-plateau-85919?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://beaubarrier:$Cowleg$17@cluster0.hxmal.mongodb.net/fierce-plateau-85919?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
